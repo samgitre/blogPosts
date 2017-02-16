aa24e68fbff955d38a5fd3dd9028945ab1aaa4a5
samgitre <samgitrepos@gmail.com>
2017-02-16 17:36:17 +0700

    Update index.js

--- dist/config/index.js
+++ dist/config/index.js
@@ -5,6 +5,6 @@ Object.defineProperty(exports, "__esModule", {
 });
 exports.default = {
   "port": 4000,
-  "dbConString": "mongodb://bitsy_soft:dy13lsd7425@ds139879.mlab.com:39879/bitsyblog"
+  "dbConString": "mongodb://localhost/bitsyblog"
 };
 //# sourceMappingURL=index.js.map
